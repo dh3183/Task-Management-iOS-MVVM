1602827d740c301c372d19e7cb0e87372f3168de
Elbin <dh3183@naver.com>
2023-02-16 10:59:24 +0900

    🪛[chore]: fetchCurrentWeek

--- TaskManagement/TaskManagement/ViewModel/TaskViewModel.swift
+++ TaskManagement/TaskManagement/ViewModel/TaskViewModel.swift
@@ -20,4 +20,29 @@ class TaskViewModel: ObservableObject {
         Task(taskTitle: "추 후 프로젝트", taskDescription: "팀과 다음 프로젝트에 대해 논의", taskDate: .init(timeIntervalSince1970: 1641677897)),
         Task(taskTitle: "앱 제안", taskDescription: "다음 앱 제안을 위해 클라이언트를 만남", taskDate: .init(timeIntervalSince1970: 1641681497)),
     ]
+    
+    // MARK: Current Week Days
+    @Published var currentWeek: [Date] = []
+    
+    // MARK: Intializing
+    init() {
+        fetchCurrentWeek()
+    }
+    
+    func fetchCurrentWeek() {
+        let today = Date()
+        let calendar = Calendar.current
+        
+        let week = calendar.dateInterval(of: .weekOfMonth, for: today)
+        
+        guard let firstWeekDay = week?.start else {
+            return
+        }
+        
+        (1...7).forEach { day in
+            if let weekday = calendar.date(byAdding: .day, value: day, to: firstWeekDay) {
+                currentWeek.append(weekday)
+            }
+        }
+    }
 }
