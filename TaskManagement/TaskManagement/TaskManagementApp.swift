c2489c3a6d29b82becb5a81db010b8e57897d3a4
Elbin <dh3183@naver.com>
2023-02-16 10:00:32 +0900

    ➕[add]: Create Xcode Project

---
+++ TaskManagement/TaskManagement/TaskManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagementApp.swift
+//  TaskManagement
+//
+//  Created by 한빈 on 2023/02/16.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
