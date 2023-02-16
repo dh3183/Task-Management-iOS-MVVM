6ffb93312bd33cff9931614a3effe8833ce0635c
Elbin <dh3183@naver.com>
2023-02-16 10:05:02 +0900

    ➕[add]: Create Swift files and grouping for Model and ViewModel

---
+++ TaskManagement/TaskManagement/Model/TaskModel.swift
@@ -0,0 +1,10 @@
+//
+//  TaskModel.swift
+//  TaskManagement
+//
+//  Created by 한빈 on 2023/02/16.
+//
+
+import SwiftUI
+
+
---
+++ TaskManagement/TaskManagement/ViewModel/TaskViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  TaskViewModel.swift
+//  TaskManagement
+//
+//  Created by 한빈 on 2023/02/16.
+//
+
+import SwiftUI
+
+struct TaskViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TaskViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        TaskViewModel()
+    }
+}
